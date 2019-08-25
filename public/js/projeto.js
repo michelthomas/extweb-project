2862014bdf15d6236f31fdf085d4ec8a65a7d2c9
Márcio Henrique <ikke.guerreiro@gmail.com>
2019-08-25 17:09:57 -0300

    Dinamização da tela de listagem de projetos; Mudança no estilo da página de listagem

--- public/js/projeto.js
+++ public/js/projeto.js
@@ -5,131 +5,120 @@ const appendProjeto = (projeto) => {
 
     // TODO id_acao ???
     console.log(JSON.stringify(projeto));
+
     document.getElementById("titulo").innerText = titulo;
 
-    document.getElementById("area_principal").innerText = area_principal;
-    document.getElementById("tipo_acao").innerText = tipo_acao;
+    document.getElementById("area_principal").innerText = "Área: " + area_principal;
+    document.getElementById("tipo_acao").innerText = "Tipo: " + tipo_acao;
 
-    /**
-    * document.getElementById("local").innerText = local;
-    * 
-    * ou
-    * 
-    * document.getElementById("local_municipio").innerText = local.municipio;
-    * document.getElementById("local_estado").innerText = local.estado;
-    */
-    document.getElementById("local_municipio").innerText = local.municipio;
-    document.getElementById("local_estado").innerText = local.estado;
 
-    document.getElementById("espaco_realizacao").innerText = espaco_realizacao;
-    document.getElementById("tipo_curso").innerText = tipo_curso;
-    document.getElementById("modalidade").innerText = modalidade;
-    document.getElementById("resumo").innerText = resumo;
+    //set images on carousel
+    var carousel = document.getElementById("carrossel");
+    var slide_list = document.getElementById("slideList");
+    imagens.forEach((imagem, i) => {
+       console.log(imagem.link);
+       // <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+       var carr = document.createElement("div");
+       var slide = document.createElement("li");
 
-    /**
-    * document.getElementById("periodo_inscricao").innerText = periodo_inscricao;
-    * 
-    * ou
-    * 
-    * document.getElementById("periodo_inscricao_data_inicial").innerText = periodo_inscricao.data_inicial;
-    * document.getElementById("periodo_inscricao_data_final").innerText = periodo_inscricao.data_final;
-    */
-    document.getElementById("periodo_inscricao_data_inicial").innerText = periodo_inscricao.data_inicial;
-    document.getElementById("periodo_inscricao_data_final").innerText = periodo_inscricao.data_final;
+       carr.classList.add("carousel-item");
+       slide.setAttribute("data-target", "carouselExampleIndicators");
+       slide.setAttribute("data-slide-to", i);
+       
+       if (i == 0)
+       {
+           carr.classList.add("active");
+           slide.classList.add("active");
+       }
 
-    /**
-    * document.getElementById("vagas").innerText = vagas;
-    * 
-    * ou
-    * 
-    * document.getElementById("vagas_quantidade_total").innerText = vagas;
-    * document.getElementById("vagas_quantidade_ocupada").innerText = vagas;
-    */
-    document.getElementById("vagas_quantidade_total").innerText = vagas;
-    document.getElementById("vagas_quantidade_ocupada").innerText = vagas;
+       var link = document.createElement("img");
+       link.setAttribute("src", imagem.link);
+       link.setAttribute("height", 500);
+       link.setAttribute("width", 1000);
+       link.setAttribute("class", "d-block w-100");
 
-    /**
-    * document.getElementById("programacao").innerText = programacao;
-    * 
-    * ou
-    * 
-    * document.getElementById("programacao_data").innerText = programacao.data;
-    * document.getElementById("programacao_horario_inicial").innerText = programacao.horario_inicial;
-    * document.getElementById("programacao_horario_final").innerText = programacao.horario_final;
-    * document.getElementById("programacao_atividades").innerText = programacao.atividades;
-    */
-    document.getElementById("programacao_data").innerText = programacao.data;
-    document.getElementById("programacao_horario_inicial").innerText = programacao.horario_inicial;
-    document.getElementById("programacao_horario_final").innerText = programacao.horario_final;
-    document.getElementById("programacao_atividades").innerText = programacao.atividades;
+       carr.appendChild(link);
+       carousel.appendChild(carr);
+       slide_list.appendChild(slide);
 
-    publico_alvo.map((publico) => {
-    // TODO criar elemento e dar append (Qual elemento?)
-    document.getElementById("publico_alvo").innerText = publico;
     });
 
+    document.getElementById("resumo").innerText = resumo;
 
-    document.getElementById("carga_horaria").innerText = carga_horaria;
+    const ul = document.getElementById("list_publico_alvo");    
+    publico_alvo.map((publico) => {
+        const li = document.createElement("li");
+        li.innerText = publico;
+        ul.appendChild(li);
 
+    });
+
+    document.getElementById("cidade_estado").innerText = local.municipio + '/' + local.estado;
+    document.getElementById("espaco_localizacao").innerText = espaco_realizacao;
+
+    document.getElementById("periodo_inscricao").innerText = "De " + periodo_inscricao.data_inicial + " à " + periodo_inscricao.data_final;
+    var vagas_disponiveis =  vagas.quantidade_total - vagas.quantidade_ocupada;
+    document.getElementById("vagas_disponiveis").innerText = "Vagas Disponíveis: " + vagas_disponiveis;
+
+    if (vagas_disponiveis > 0)
+    {
+        const div_inscricao = document.getElementById("div_inscricao");
+        const btn = document.createElement("button");
+        btn.classList.add("btn");
+        btn.classList.add("btn-dark");
+        btn.innerText = "Realizar Inscrição";
+
+        div_inscricao.appendChild(btn);
+
+    }
+
+    // document.getElementById("tipo_curso").innerText = tipo_curso;
+    // document.getElementById("modalidade").innerText = modalidade;
+
+    const list_programacao = document.getElementById("list_programacao");
+    programacao.map((atual) => {
+        const tr = document.createElement("tr");
+        const data = document.createElement("td");
+        const horario = document.createElement("td");
+        const atividades = document.createElement("td");
+
+        data.innerText = atual.data;
+        horario.innerText = atual.horario_inicial + " - " + atual.horario_final;
+        atividades.innerText = atual.atividades;
+
+        tr.appendChild(data);
+        tr.appendChild(horario);
+        tr.appendChild(atividades);
+        list_programacao.appendChild(tr);
+    });
+
+    //document.getElementById("carga_horaria").innerText = carga_horaria;
+
+    const contatos = document.getElementById("contatos");
     equipe.map((pessoa) => {
 
-    //TODO appendar na div da equipe, as divs das pessoas     
-    // TODO Mudar os elementos aqui, depois do front quais serão
-    const elemento_nome = document.createElement("h2");
+    const elemento_nome = document.createElement("h5");
     elemento_nome.innerText = pessoa.nome;
 
-    const elemento_categoria = document.createElement("h2");
-    elemento_categoria.innerText = pessoa.categoria;
+    const ul = document.createElement("ul");
 
-    const elemento_funcao = document.createElement("h2");
-    elemento_funcao.innerText = pessoa.funcao;
+    const elemento_categoria = document.createElement("li");
+    elemento_categoria.innerText = "Categoria: " + pessoa.categoria;
 
-    const elemento_email = document.createElement("h2");
-    elemento_email.innerText = pessoa.email;
+    const elemento_funcao = document.createElement("li");
+    elemento_funcao.innerText = "Função: " + pessoa.funcao;
 
-    const div_pessoa = document.createElement("div");
+    const elemento_email = document.createElement("li");
+    elemento_email.innerText = "E-mail: " + pessoa.email;
 
-    div_pessoa.appendChild(elemento_nome);
-    div_pessoa.appendChild(elemento_categoria);
-    div_pessoa.appendChild(elemento_funcao);
-    div_pessoa.appendChild(elemento_email);
+    
+    ul.appendChild(elemento_categoria);
+    ul.appendChild(elemento_funcao);
+    ul.appendChild(elemento_email);
 
-    document.getElementById("equipe").appendChild(div_pessoa);
+    contatos.appendChild(elemento_nome);
+    contatos.appendChild(ul);
 });
-
-
-//TODO carregar imagens
-
-/*//set images on carousel
-var carousel = document.getElementById("carrossel");
-var slide_list = document.getElementById("slideList");
-imagens.forEach((imagem, i) => {
-   console.log(imagem.link);
-   // <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-   var carr = document.createElement("div");
-   var slide = document.createElement("li");
-
-   carr.classList.add("carousel-item");
-   slide.setAttribute("data-target", "carouselExampleIndicators");
-   slide.setAttribute("data-slide-to", i);
-   
-   if (i == 0)
-   {
-       carr.classList.add("active");
-       slide.classList.add("active");
-   }
-
-   var link = document.createElement("img");
-   link.setAttribute("src", imagem.link);
-   link.setAttribute("height", 500);
-   link.setAttribute("width", 1000);
-   link.setAttribute("class", "d-block w-100");
-
-   carr.appendChild(link);
-   carousel.appendChild(carr);
-   slide_list.appendChild(slide);
-
-});*/
 
 
 publicoAlvo.forEach(publico => {
@@ -143,7 +132,7 @@ const appendProjectsData = (data) => {
 }
 
 const carregarProjeto = (id) => {
-    const projeto = projetoArr.find((projeto) => projeto.id_acao === id);
+    const projeto = projetoArr.find((projeto) => projeto.id_acao === parseInt(id));
     appendProjeto(projeto);
 }
 
