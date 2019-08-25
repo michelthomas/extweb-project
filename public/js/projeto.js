104d46319330c5eb29b9754d5b4670916b773b2c
michelthomas <michel.lins17@gmail.com>
2019-08-25 05:09:47 -0300

    Implementacao de parte das funcionalidades definidas pelo patrao; Reestruturacao...

--- public/js/projeto.js
+++ public/js/projeto.js
@@ -1,156 +1,205 @@
-class Projeto {
-    constructor(titulo, areaPrincipal, tipoDeAcao, resumo, local, publicoAlvo, vagas, equipe, imagens) {
-        this._titulo = titulo;
-        this._areaPrincipal = areaPrincipal;
-        this._tipoDeAcao = tipoDeAcao;
-        this._resumo = resumo;
-        this._local = local;
-        this._publicoAlvo = publicoAlvo;
-        this._vagas = vagas;
-        this._equipe = equipe;
-        this._imagens = imagens;
-    }
+const appendProjeto = (projeto) => {
+    const {id_acao, titulo, area_principal, tipo_acao, local, espaco_realizacao,
+        tipo_curso, modalidade, resumo, periodo_inscricao, vagas, programacao, publico_alvo,
+        carga_horaria, equipe, imagens} = projeto;
 
-    get titulo() {
-        return this._titulo;
-    }
-    set titulo(novoTitulo) {
-        this._titulo = novoTitulo;
-    }
-    
-    get areaPrincipal() {
-        return this._areaPrincipal;
-    }
-    set areaPrincipal(novoAreaPrincipal) {
-        this._areaPrincipal = novoAreaPrincipal;
-    }
-    
-    get tipoDeAcao() {
-        return this._tipoDeAcao;
-    }
-    set tipoDeAcao(novoTipoDeAcao) {
-        this._tipoDeAcao = novoTipoDeAcao;
-    }
+    // TODO id_acao ???
+    console.log(JSON.stringify(projeto));
+    document.getElementById("titulo").innerText = titulo;
 
-    get resumo() {
-        return this._resumo;
-    }
-    set resumo(novoResumo) {
-        this._resumo = novoResumo;
-    }
-    
-    get local() {
-        return this._local;
-    }
-    set local(novoLocal) {
-        this._local = novoLocal;
-    }
-    
-    get publicoAlvo() {
-        return this._publicoAlvo;
-    }
-    set publicoAlvo(novoPublicoAlvo) {
-        this._publicoAlvo = novoPublicoAlvo;
-    }
-    
-    get vagas() {
-        return this._vagas;
-    }
-    set vagas(novoVagas) {
-        this._vagas = novoVagas;
-    }
-    
-    get equipe() {
-        return this._equipe;
-    }
-    set equipe(novoEquipe) {
-        this._equipe = novoEquipe;
-    }
-    
-    get imagens() {
-        return this._imagens;
-    }
-    set imagens(novoImagens) {
-        this._imagens = novoImagens;
-    }
+    document.getElementById("area_principal").innerText = area_principal;
+    document.getElementById("tipo_acao").innerText = tipo_acao;
 
-};
+    /**
+    * document.getElementById("local").innerText = local;
+    * 
+    * ou
+    * 
+    * document.getElementById("local_municipio").innerText = local.municipio;
+    * document.getElementById("local_estado").innerText = local.estado;
+    */
+    document.getElementById("local_municipio").innerText = local.municipio;
+    document.getElementById("local_estado").innerText = local.estado;
 
-const projeto = new Projeto("Projeto 1", "Area principaldde nada",
- "acao de tipo 4", "lorem ipslum aoooo", "ufal", ["pessoas", "selvagens"], "vafas", "equipe competente", "src de imagens");
+    document.getElementById("espaco_realizacao").innerText = espaco_realizacao;
+    document.getElementById("tipo_curso").innerText = tipo_curso;
+    document.getElementById("modalidade").innerText = modalidade;
+    document.getElementById("resumo").innerText = resumo;
 
-const projetoArr = [projeto];
+    /**
+    * document.getElementById("periodo_inscricao").innerText = periodo_inscricao;
+    * 
+    * ou
+    * 
+    * document.getElementById("periodo_inscricao_data_inicial").innerText = periodo_inscricao.data_inicial;
+    * document.getElementById("periodo_inscricao_data_final").innerText = periodo_inscricao.data_final;
+    */
+    document.getElementById("periodo_inscricao_data_inicial").innerText = periodo_inscricao.data_inicial;
+    document.getElementById("periodo_inscricao_data_final").innerText = periodo_inscricao.data_final;
 
-const getProjetos = () => projetoArr;
+    /**
+    * document.getElementById("vagas").innerText = vagas;
+    * 
+    * ou
+    * 
+    * document.getElementById("vagas_quantidade_total").innerText = vagas;
+    * document.getElementById("vagas_quantidade_ocupada").innerText = vagas;
+    */
+    document.getElementById("vagas_quantidade_total").innerText = vagas;
+    document.getElementById("vagas_quantidade_ocupada").innerText = vagas;
 
-const getProjeto = () => {
-    appendProjectsData(getProjetos());
+    /**
+    * document.getElementById("programacao").innerText = programacao;
+    * 
+    * ou
+    * 
+    * document.getElementById("programacao_data").innerText = programacao.data;
+    * document.getElementById("programacao_horario_inicial").innerText = programacao.horario_inicial;
+    * document.getElementById("programacao_horario_final").innerText = programacao.horario_final;
+    * document.getElementById("programacao_atividades").innerText = programacao.atividades;
+    */
+    document.getElementById("programacao_data").innerText = programacao.data;
+    document.getElementById("programacao_horario_inicial").innerText = programacao.horario_inicial;
+    document.getElementById("programacao_horario_final").innerText = programacao.horario_final;
+    document.getElementById("programacao_atividades").innerText = programacao.atividades;
+
+    publico_alvo.map((publico) => {
+    // TODO criar elemento e dar append (Qual elemento?)
+    document.getElementById("publico_alvo").innerText = publico;
+    });
+
+
+    document.getElementById("carga_horaria").innerText = carga_horaria;
+
+    equipe.map((pessoa) => {
+
+    //TODO appendar na div da equipe, as divs das pessoas     
+    // TODO Mudar os elementos aqui, depois do front quais serão
+    const elemento_nome = document.createElement("h2");
+    elemento_nome.innerText = pessoa.nome;
+
+    const elemento_categoria = document.createElement("h2");
+    elemento_categoria.innerText = pessoa.categoria;
+
+    const elemento_funcao = document.createElement("h2");
+    elemento_funcao.innerText = pessoa.funcao;
+
+    const elemento_email = document.createElement("h2");
+    elemento_email.innerText = pessoa.email;
+
+    const div_pessoa = document.createElement("div");
+
+    div_pessoa.appendChild(elemento_nome);
+    div_pessoa.appendChild(elemento_categoria);
+    div_pessoa.appendChild(elemento_funcao);
+    div_pessoa.appendChild(elemento_email);
+
+    document.getElementById("equipe").appendChild(div_pessoa);
+});
+
+
+//TODO carregar imagens
+
+/*//set images on carousel
+var carousel = document.getElementById("carrossel");
+var slide_list = document.getElementById("slideList");
+imagens.forEach((imagem, i) => {
+   console.log(imagem.link);
+   // <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+   var carr = document.createElement("div");
+   var slide = document.createElement("li");
+
+   carr.classList.add("carousel-item");
+   slide.setAttribute("data-target", "carouselExampleIndicators");
+   slide.setAttribute("data-slide-to", i);
+   
+   if (i == 0)
+   {
+       carr.classList.add("active");
+       slide.classList.add("active");
+   }
+
+   var link = document.createElement("img");
+   link.setAttribute("src", imagem.link);
+   link.setAttribute("height", 500);
+   link.setAttribute("width", 1000);
+   link.setAttribute("class", "d-block w-100");
+
+   carr.appendChild(link);
+   carousel.appendChild(carr);
+   slide_list.appendChild(slide);
+
+});*/
+
+
+publicoAlvo.forEach(publico => {
+   console.log(publico);
+});
 }
 
 //set attributes from json to html with creating html elements
-appendProjectsData = (data) => {
-    data.forEach(projeto => {
+const appendProjectsData = (data) => {
+    data.forEach(projeto => appendProjeto(projeto));
+}
 
-        const { titulo, areaPrincipal, tipoAcao, tipo_curso, modalidade, resumo, 
-            local, publicoAlvo, vagas, equipe, imagens} = projeto;
+const carregarProjeto = (id) => {
+    const projeto = projetoArr.find((projeto) => projeto.id_acao === id);
+    appendProjeto(projeto);
+}
 
-        document.getElementById("titulo").innerText = titulo;
+const carregarTodosProjetos = () => {
+    appendProjectsData(projetoArr);
+}
 
-        document.getElementById("txtDescricao").innerText = resumo;
+// TODO Filtrar (?)
 
-        /*//set images on carousel
-        var carousel = document.getElementById("carrossel");
-        var slide_list = document.getElementById("slideList");
-        imagens.forEach((imagem, i) => {
-            console.log(imagem.link);
-            // <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            var carr = document.createElement("div");
-            var slide = document.createElement("li");
+const calcularNumeroTotalDeProjetos = () => (projetoArr.length)
 
-            carr.classList.add("carousel-item");
-            slide.setAttribute("data-target", "carouselExampleIndicators");
-            slide.setAttribute("data-slide-to", i);
-            
-            if (i == 0)
-            {
-                carr.classList.add("active");
-                slide.classList.add("active");
-            }
+const calcularNumeroDeParticipantes = (id) => {
+    const projeto = projetoArr.find((projeto) => projeto.id_acao === id);
+    return projeto.equipe.length;
+}
 
-            var link = document.createElement("img");
-            link.setAttribute("src", imagem.link);
-            link.setAttribute("height", 500);
-            link.setAttribute("width", 1000);
-            link.setAttribute("class", "d-block w-100");
+// Por projeto?
+const calcularNumeroDeParticipantesDiscentes = (id) => {
+    const projeto = projetoArr.find(projeto => projeto.id_acao === id);
+    return projeto.equipe.filter(pessoa => pessoa.categoria === "Discente").length;
+}
 
-            carr.appendChild(link);
-            carousel.appendChild(carr);
-            slide_list.appendChild(slide);
+const calcularNumeroDeParticipantesDocentes = (id) => {
+    const projeto = projetoArr.find(projeto => projeto.id_acao === id);
+    return projeto.equipe.filter(pessoa => pessoa.categoria === "Docente").length;
+}
 
-        });*/
+// Só discente recebe bolsa?
+const calcularNumeroDeBolsas = (id) => {
+    return calcularNumeroDeParticipantesDiscentes(id);
+}
 
+const calcularValorTotalPago = (id) => {
+    // TODO Verificar esse preco. É algo fixo? É por projeto? Se for por projeto, acrescentar no json, ou solicitar agr(prompt)
+    const precoBolsa = 0;
+    return calcularNumeroDeBolsas(id) * precoBolsa;
+}
 
-        publicoAlvo.forEach(publico => {
-            console.log(publico);
-        });
+// Inscrição de quem? Da equipe? Se for de um participante, n precisa disso aqui, somente mexer nas vagas
+const realizarInscricao = (id) => {
+    const nome = prompt("Digite seu nome:");
+    const categoria = prompt("Digite sua categoria:");
+    const funcao = prompt("Digite sua funcao:");
+    const email = prompt("Digite seu email:");
 
-        // const b = document.createElement('b');
-        // const p1 = document.createElement('p');
-        // const p2 = document.createElement('p');
-        // const p3 = document.createElement('p');
-        //
-        // b.textContent = phone.name;
-        // p2.textContent = "Número: " + phone.display_number;
-        // p3.textContent = "Local: " + phone.location;
-        //
-        // lista.appendChild(item);
-        // p1.appendChild(b);
-        // item.appendChild(p1);
-        // item.appendChild(p2);
-        // item.appendChild(p3);
+    const projeto = projetoArr.find(projeto => projeto.id_acao === id);
 
-        // document.getElementById("tipo_curso").innerText = tipo_curso;
-        // document.getElementById("modalidade").innerText = modalidade;
-        // document.getElementById("resumo").innerText = resumo;
-    });
+    projeto.equipe.push({nome, categoria, funcao, email});
+
+}
+
+// o que isso faz? Deve ser para mexer no DOM...
+const atualizarInscricoes = (id) => {
+    const projeto = projetoArr.find(projeto => projeto.id_acao === id);
+
+    projeto.vagas.quantidade_total--;
+
+    appendProjeto(projeto);
 }
